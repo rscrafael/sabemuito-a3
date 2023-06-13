5e0cce19574d1eaa60b685f455ab23d0ad36ccb5
Rafael Soares Cardozo <rscrafael@gmail.com>
2023-06-12 21:41:18 -0300

    Adicionado a route topico

---
+++ routes/topico.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import pool from '../pool.js';
+
+const routes = express.Router();
+
+routes.get("/topico/:id_usuario",  (req, res, error) => {
+    const sql = 'SELECT * FROM topico WHERE `id_usuario` = ?';
+    pool.query(sql, [req.params.id_usuario], (error, results, fields ) => {
+        if(!error){
+            res.status(200).json(results);
+        } else {
+            res.status(404).json({msg: "Sem dados"});
+        }
+    });
+});
+
+export default routes;
